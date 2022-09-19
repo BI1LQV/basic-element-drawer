2994bfbd68f711808231afd14df50a3951a76a5e
bi1lqv <bi1lqv.y@gmail.com>
2022-09-19 12:19:41 +0800

    test: 把inlinesnapshot抽到外面去

--- src/utils/drawLine.spec.ts
+++ src/utils/drawLine.spec.ts
@@ -3,53 +3,7 @@ import { DDALine } from "./drawLine"
 
 describe("draw line", () => {
   it("dda should works", () => {
-    expect(DDALine(3, 3, 9, 13)).toMatchInlineSnapshot(`
-      [
-        [
-          3,
-          3,
-        ],
-        [
-          4,
-          4,
-        ],
-        [
-          4,
-          5,
-        ],
-        [
-          5,
-          6,
-        ],
-        [
-          5,
-          7,
-        ],
-        [
-          6,
-          8,
-        ],
-        [
-          7,
-          9,
-        ],
-        [
-          7,
-          10,
-        ],
-        [
-          8,
-          11,
-        ],
-        [
-          8,
-          12,
-        ],
-        [
-          9,
-          13,
-        ],
-      ]
-    `)
+    expect(DDALine(3, 3, 9, 13)).toMatchSnapshot()
+    expect(DDALine(1, 1, 6, 5)).toMatchSnapshot()
   })
 })
