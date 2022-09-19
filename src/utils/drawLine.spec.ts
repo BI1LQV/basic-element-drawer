e49780780bb7c07e0e2a4acd9fda639cf3f833d3
bi1lqv <bi1lqv.y@gmail.com>
2022-09-19 12:24:19 +0800

    test: dda增加测试

--- src/utils/drawLine.spec.ts
+++ src/utils/drawLine.spec.ts
@@ -5,5 +5,8 @@ describe("draw line", () => {
   it("dda should works", () => {
     expect(DDALine(3, 3, 9, 13)).toMatchSnapshot()
     expect(DDALine(1, 1, 6, 5)).toMatchSnapshot()
+    expect(DDALine(1, 1, 6, 8)).toMatchSnapshot()
+    expect(DDALine(9, 13, 3, 3)).toMatchSnapshot()
+    expect(DDALine(-3, 3, -13, 11)).toMatchSnapshot()
   })
 })
