e7585fb449b803469063f0f83d208999cb1b387a
bi1lqv <bi1lqv.y@gmail.com>
2022-09-21 09:32:56 +0800

    test: 画圆测试

--- src/utils/drawCirc.spec.ts
+++ src/utils/drawCirc.spec.ts
@@ -3,38 +3,7 @@ import { centerCirc, circCore } from "./drawCirc"
 
 describe("draw circ", () => {
   it("should works", () => {
-    expect(circCore(3)).toMatchInlineSnapshot(`
-      [
-        [
-          0,
-          3,
-        ],
-        [
-          1,
-          3,
-        ],
-        [
-          2,
-          2,
-        ],
-      ]
-    `)
-    const circ = centerCirc(5, 5, 3)
-    expect(circ.next().value).toMatchInlineSnapshot(`
-      [
-        [
-          5,
-          8,
-        ],
-        [
-          6,
-          8,
-        ],
-        [
-          7,
-          7,
-        ],
-      ]
-    `)
+    expect(circCore(3)).toMatchSnapshot()
+    expect(Array.from(centerCirc(5, 5, 3))).toMatchSnapshot()
   })
 })
