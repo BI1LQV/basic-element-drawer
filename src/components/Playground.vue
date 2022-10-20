082d5b2968a2a73eb1a855e355ee7974e5d4f1b9
BI1LQV <bi1lqv.y@gmail.com>
2022-10-20 16:32:11 +0800

    chore: 删没用代码

--- src/components/Playground.vue
+++ src/components/Playground.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { PixelState } from "@/model"
-import { allowClick, clickedPoint, playgroundState, requestPoint, sizeX, sizeY } from "@/store"
+import { allowClick, clickedPoint, playgroundState, sizeX, sizeY } from "@/store"
 import { pixelToIdx } from "@/utils"
 const STATE_COLOR_MAP = {
   [PixelState.empty]: "bg-gray-500/10",
@@ -8,33 +8,15 @@ const STATE_COLOR_MAP = {
   [PixelState.fill]: "bg-yellow-500/50",
   [PixelState.selected]: "bg-blue-500/50",
 }
-// let isDrawing = false
+
 function drawPixel(x: number, y: number) {
-  // if (!isDrawing) { return }
   if (!allowClick.value) { return }
   clickedPoint.value = `${x}_${y}`
   playgroundState.value[pixelToIdx(x, y)] = PixelState.selected
 }
-
-// function startDraw() {
-//   isDrawing = true
-// }
-// function endDraw() {
-//   isDrawing = false
-// }
 </script>
 
 <template>
-  <!-- <div flex flex-col @mousedown="startDraw" @mouseup="endDraw">
-    <div v-for="x of sizeX" :key="x" flex flex-grow flex-row>
-      <div
-        v-for="y of sizeY" :key="y"
-        flex-grow class="m-0.2"
-        :class="STATE_COLOR_MAP[ playgroundState[(x - 1) * sizeY + (y - 1)] ]"
-        @mouseover="drawPixel((x - 1) * sizeY + (y - 1))"
-      ></div>
-    </div>
-  </div> -->
   <div flex flex-col>
     <div v-for="x of sizeX" :key="x" flex flex-grow flex-row>
       <div
